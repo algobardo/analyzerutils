9458b9a852db10b7518dda44cb7da71fbb841df5
christofferqa <christofferqa@gmail.com>
2016-04-21 20:33:36 -0700

    Defaults

--- lib/options.dart
+++ lib/options.dart
@@ -1,5 +1,6 @@
 library dart_annotation_levels.options;
 
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:args/args.dart';
@@ -37,13 +38,27 @@ class SemanticCommandOptions {
   }
 
   static void createOptions(ArgParser a) {
+    a.addOption('benchmark', help: "Benchmark name");
     a.addOption('dir', help: "Project directory");
     a.addOption('entry', help: "Project entry point", allowMultiple: true);
     a.addFlag('inline', help: "Inline packages", defaultsTo: false);
   }
 
-
-  SemanticCommandOptions.parse(ArgResults opts) : this.args(opts["dir"], opts["entry"], opts["inline"]);
+  SemanticCommandOptions.parse(ArgResults opts, [ bool desugar = false ]) {
+    if (opts["benchmark"] != null) {
+      Map projects = JSON.decode(new File("./benchmarks/projects.json").readAsStringSync());
+      Map project = projects[opts["benchmark"]];
+      this.projectDirectory = "benchmarks/${desugar ? "original" : "desugared"}/${project["package"]}";
+      this.entryFile = [project["entry"]];
+    }
+    if (opts["dir"] != null)
+      this.projectDirectory = opts["dir"];
+    if (opts["entry"] != null)
+      this.entryFile = opts["entry"];
+    this.inline = opts["inline"];
+    if(projectDirectory == null || entryFile == null)
+      throw new UsageException("Options 'dir' and 'entry' are mandatory", "--dir <directory> --entry <entry>");
+  }
 
 }
 
@@ -53,11 +68,11 @@ class DesugarCommandOptions extends SemanticCommandOptions {
   bool ignoreCache;
   bool push = true; //TODO: See if this can be enabled by default considering the casa machines
 
-  DesugarCommandOptions.args(this.destination, this.ignoreCache, ArgResults o) : super.parse(o);
+  DesugarCommandOptions.args(this.destination, this.ignoreCache, ArgResults o) : super.parse(o, true);
 
   static void createOptions(ArgParser a) {
     SemanticCommandOptions.createOptions(a);
-    a.addOption('dest', help: "Destination Folder");
+    a.addOption('dest', help: "Destination Folder", defaultsTo: "benchmarks/desugared");
     a.addFlag('ignore-cache', help: "Ignore github cache branch", defaultsTo: true);
   }
 
